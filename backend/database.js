36e7c04c59027dee7d848331143ab719477aa838
poornima63 <poornima63061@gmail.com>
2025-07-30 11:45:31 +0530

    add start script

--- backend/database.js
+++ backend/database.js
@@ -7,6 +7,7 @@ const connectToMongoDB = async () => {
     try {
         console.log("Mongo URI is:", process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI)
+        console.log("mongoDB connected successfully")
        
     } catch (error) {
         console.log("MongoDB connection failed!", error)
